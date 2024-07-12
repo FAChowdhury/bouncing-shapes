0c388282a698e38ac1773d05b0eb6668222e7add
farhan <chowdhury.farhan235@gmail.com>
2024-07-12 12:56:02 +1000

    removed precommit conditions

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 
 int main()
 {
-					  int lol = 0;
 	std::cout << "SFML is working!" << std::endl;
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	sf::CircleShape shape(100.f);
