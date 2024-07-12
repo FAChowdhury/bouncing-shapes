944965dfdb3b48c1c9073b9b6a7ee6c3fb6802c1
farhan <chowdhury.farhan235@gmail.com>
2024-07-12 11:45:57 +1000

    enforced clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,24 @@
 
 int main()
 {
-    std::cout << "SFML is working!" << std::endl;
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	std::cout << "SFML is working!" << std::endl;
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
 
-    return 0;
+	return 0;
 }
