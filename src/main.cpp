0e3c6f7807ac5acd62cab4f00a26a7ee0be1f391
farhan <chowdhury.farhan235@gmail.com>
2024-07-14 13:49:40 +1000

    can read in window W and  H

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,39 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <limits>
 
 int main()
 {
-	std::cout << "SFML is working!" << std::endl;
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	// read in the config
+	auto fin = std::ifstream("../src/config.txt"); // this is relative path from build folder
+	if (fin.fail()) {
+		std::cout << "error opening file" << std::endl;
+		return -1;
+	}
+
+	// read window properties
+	std::string word;
+	fin >> word;
+	unsigned int win_width = 100;
+	unsigned int win_height = 100;
+	fin >> win_width;
+	fin >> win_height;
+
+	// read shapes
+	while (fin >> word) {
+		if (word == "Circle") {
+			std::cout << "It's a circle" << std::endl;
+			break;
+		} else if (word == "Rectangle") {
+			std::cout << "It's a Rectangle" << std::endl;
+			break;
+		}
+	}
+
+	// render shapes
+	sf::RenderWindow window(sf::VideoMode(win_width, win_height), "SFML works!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
 
